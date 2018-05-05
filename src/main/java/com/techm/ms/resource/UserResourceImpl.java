bbddb885f2f739c8bf23299c004b47bec6c25439
rraj <rtraj88@gmail.com>
2018-05-05 13:16:06 -0500

    Implemented UserResource

--- src/main/java/com/techm/ms/resource/UserResourceImpl.java
+++ src/main/java/com/techm/ms/resource/UserResourceImpl.java
@@ -1,8 +1,50 @@
 package com.techm.ms.resource;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.stereotype.Component;
+
+import com.techm.ms.model.User;
+
+@Component
 public class UserResourceImpl implements UserResource {
+
+	
+	public static List<User> list;
+	
+	static {
+		//User u = new User(1, "rituraj",20, 1111);
+		
+	}
+	
+	@Override
+	public String CreateUser(long id,String name, int age,int accountid) {
+		
+		for(User user : list) {
+			if(user.getName().equals(name)) {
+				return null;
+			}
+		}
+		
+		User us = new User(id, name, age, accountid);
+		
+		list.add(us);
+		
+		return list.toString();
+		
+	}
+
+	@Override
+	public String GetUser(long Id) {
+		
+		for(User ur : list) {
+			if(ur.getId() == Id) {
+				return ur.toString();
+			}
+		}
+		return null;
+		
+		
+	}
 	
 }
---
+++ src/test/java/com/techm/ms/component/UserTest.java
@@ -0,0 +1,60 @@
+package com.techm.ms.component;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.techm.ms.resource.UserController;
+import com.techm.ms.resource.UserResourceImpl;
+
+public class UserTest {
+	
+	@InjectMocks
+	public UserController uc;
+	
+	@Before
+	public void initMocks() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void addUserifNotExists() {
+		UserResourceImpl uri = new UserResourceImpl();
+		ReflectionTestUtils.setField(uc,"us",uri);
+		ResponseEntity<String> r = uc.addUser(1214,"ritu",25,1001);
+		assertEquals(r.getStatusCode(),HttpStatus.ACCEPTED);
+	}
+	
+	@Test
+	public void NoaddUserifExists() {
+		UserResourceImpl uri = new UserResourceImpl();
+		ReflectionTestUtils.setField(uc,"us",uri);
+		ResponseEntity<String> r = uc.addUser(1214,"ritu",25,1001);
+		assertEquals(r.getStatusCode(),HttpStatus.CONFLICT);
+	}
+	
+	@Test
+	public void getUserifExists() {
+		UserResourceImpl uri = new UserResourceImpl();
+		ReflectionTestUtils.setField(uc,"us",uri);
+		ResponseEntity<String> r = uc.getUser(1214);
+		assertEquals(r.getStatusCode(),HttpStatus.FOUND);
+	}
+	
+	@Test
+	public void getUserifNotExists() {
+		UserResourceImpl uri = new UserResourceImpl();
+		ReflectionTestUtils.setField(uc,"us",uri);
+		ResponseEntity<String> r = uc.getUser(1219);
+		assertEquals(r.getStatusCode(),HttpStatus.NOT_FOUND);
+	}
+	
+	
+
+}
